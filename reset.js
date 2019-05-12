5e35ff00b1b1c84763ba8f5f6e75c041904ad2d3
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-12 20:51:13 +0530

    Update reset.js

--- reset.js
+++ reset.js
@@ -1,13 +1,10 @@
-exports.run = (msg,client,Discord,color,pf) => {
-   client.fetchApplication()
-   .then(p => {
-       if(msg.author.id==p.owner.id){
-          msg.channel.send(`Resetting... **${client.user.tag}**`)
-         .then(client.destroy())
-         .then(client.login(process.env.TOKEN));
-          client.on('ready', () => {
-             msg.channel.send(`**${client.user.tag}** is online! took ${parseInt(client.ping)}ms`);
-          });
-       }
-   });
+exports.run = (msg,client,Discord,color,pf,ownerID) => {
+    if(msg.author.id==ownerID){
+       msg.channel.send(`Resetting... **${client.user.tag}**`)
+       .then(client.destroy())
+       .then(client.login(process.env.TOKEN));
+        client.on('ready', () => {
+            msg.channel.send(`**${client.user.tag}** is online! took ${parseInt(client.ping)}ms`);
+        });
+     }
 }
