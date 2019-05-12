46309dc694cd25385d5374a28b102a8697b421a0
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-12 22:00:10 +0530

    Update eball.js

--- eball.js
+++ eball.js
@@ -3,9 +3,9 @@ exports.run = (msg,client,Discord,color,arg1) => {
    const nekoLifeJSON = require('./sfw.json');
    weeb.nekoLife(nekoLifeJSON.sfw.eBall, 'img').then(res => {
        var data = JSON.parse(res).url;
-      let r=new Discord.RichEmbed()
+       let r=new Discord.RichEmbed()
       .setAuthor(`Question by ${msg.author.username}`, msg.author.avatarURL)
-      .setDescription('```'+(yui)+'```')
+      .setDescription(`\`\`\`${arg1}\`\`\``)
       .setImage(data)
       .setTimestamp(new Date())
       .setColor(color);
