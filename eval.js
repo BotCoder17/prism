4d288d2019fab8b3ccfbd09c87a12eb73c7260d4
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-13 11:46:09 +0530

    Create eval.js

---
+++ eval.js
@@ -0,0 +1,26 @@
+exports.run = (msg,client,Discord,color,ownerID) => {
+    if(msg.author.id!=ownerID) return;
+    const arrrrg = msg.content.split(" ").slice(1);
+    const chk=msg.content.split('.');
+    function clean(text) {
+        if (typeof(text) === "string")
+          return text
+                 .replace(/`/g, "`" + String.fromCharCode(8203))
+                 .replace(/@/g, "@" + String.fromCharCode(8203));
+        else return text;
+     }
+     try {
+       const code = arrrrg.join(" ");
+       let rest,mvm=eval(code);
+       if(typeof mvm!='undefined'){
+          let eevv=new Discord.RichEmbed()
+              .setColor(color)
+              rest = require("util").inspect(mvm);
+              eevv.addField('Output',`\`\`\`xl\n${clean(rest)}\`\`\``);
+           eevv.addField('Type of code',`\`\`\`xl\n${typeof mvm}\`\`\``);
+           msg.channel.send(eevv);
+        }
+    } catch (err) {
+           msg.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
