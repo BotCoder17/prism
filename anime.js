2d708d9adcaeade49ac9bc39959f832557f929e0
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-14 14:39:21 +0530

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,1 +1,22 @@
-
+exports.run = (msg,Discord,client,cmd,arg01) => {
+   const weeb = require('node-weeb');
+   const sr = require('./sfw.json');
+   if(arg01!=''){
+     client.fetchUser(arg01)
+      .then(u => {
+        switch(cmd){
+          case 'tickle':
+             weeb.nekoLife(sr.sfw.tickle, 'img')
+               .then(res => {
+            let r=new Discord.RichEmbed()
+           .setTimestamp(new Date())
+           .setTitle(`${msg.author.username} tickles u, ${u.username}`)
+           .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+           .setImage(JSON.parse(res).url)
+           .setColor(color);
+            msg.channel.send(r);
+          break;
+        }
+     }).catch(console.log);
+   }
+}
