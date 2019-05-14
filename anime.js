11c72dc82903155bdf5f97a1c1278bd772bf3042
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-14 14:55:46 +0530

    Update anime.js

--- anime.js
+++ anime.js
@@ -17,6 +17,47 @@ exports.run = (msg,Discord,client,color,cmd,arg01) => {
                   msg.channel.send(r);
                }).catch(console.log);
           break;
+          case 'feed':
+               weeb.nekoLife(sr.sfw.feed, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`Eat this, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+          break;
+          case 'cuddle':
+              weeb.nekoLife(sr.sfw.cuddle, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} cuddles u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+          break;
+          case 'neko':
+               weeb.nekoLife(sr.sfw.nekoGif, 'img')
+               .then(res => {
+                  var t=JSON.stringify(res),json='';
+                  for(var i=1;i<t.length-3;i++){
+                     let y=t.charAt(i);
+                     if(y!='\\') json+=y;
+                  }
+                  let r=new Discord.RichEmbed()
+                     .setTimestamp(new Date())
+                     .setTitle(`${msg.author.username} sent a neko for u, ${u.username}`)
+                     .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                     .setImage(JSON.parse(json).url)
+                     .setColor(color);
+                     msg.channel.send(r);
+               }).catch(console.log);
+          break;
         }
      }).catch(console.log);
    }
