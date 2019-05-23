aa5b0b5c96cc22022995dd199ea1a5cc61384d3c
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-23 18:24:46 +0530

    Update xvid.js

--- xvid.js
+++ xvid.js
@@ -1,3 +1,13 @@
-exports.run = (msg,Discord,client,arg1) => {
-    if(client.channels.get(msg.channel.id).nsfw==false) return msg.channel.send(`U can view this only in NSFW enabled channels`);
+exports.run = (msg,Discord,client,arg1,color) => {
+    if(!client.channels.get(msg.channel.id).nsfw) return msg.channel.send(`U can view this only in NSFW enabled channels`);
+    const Pornsearch = require('pornsearch');
+     const Searcher = new Pornsearch('fuck');
+     Searcher.gifs()
+       .then(gifs => {
+            let gif=new Discord.RichEmbed()
+            .setImage(gifs[1].url)
+            .setTitle(gifs[1].title)
+            .setColor(color)
+            msg.channel.send(gif)
+        });
 }
