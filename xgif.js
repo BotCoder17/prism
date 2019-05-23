81ff7e38480471c47400aa455b1549a961e9eb48
idris <50110894+BotCoder17@users.noreply.github.com>
2019-05-23 18:33:19 +0530

    Update xgif.js

--- xgif.js
+++ xgif.js
@@ -1,3 +1,14 @@
-exports.run = (msg,Discord,client,arg1) => {
-    if(client.channels.get(msg.channel.id).nsfw==false) return msg.channel.send(`U can view this only in NSFW enabled channels`);
+exports.run = (msg,Discord,client,arg1,color) => {
+    if(!client.channels.get(msg.channel.id).nsfw) return msg.channel.send(`U can view this only in NSFW enabled channels`);
+     const Pornsearch = require('pornsearch');
+     const Searcher = new Pornsearch('fuck');
+     Searcher.gifs()
+       .then(gifs => {
+            let g=Math.floor(Math.random()*gifs.length)+1;
+            let gif=new Discord.RichEmbed()
+            .setImage(gifs[g].url)
+            .setTitle(gifs[g].title)
+            .setColor(color)
+            msg.channel.send(gif)
+        });
 }
