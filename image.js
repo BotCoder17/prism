d1f633ea21e831664d2622a92507163921488c52
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 01:13:47 +0530

    Update image.js

--- image.js
+++ image.js
@@ -5,7 +5,6 @@ exports.run = (msg,client,Discord,color,arg1) => {
        var ran = parseInt(Math.floor(Math.random()*res.length));
      //  console.log(JSON.parse(JSON.stringify(res, null, '  ')));
        let img=JSON.parse(JSON.stringify(res, null, '  '));
-        console.log(arg1)
         let im=new Discord.RichEmbed()
         .setTitle(`Image search for ${arg1}`)
         .setImage(img[ran].url)
@@ -13,10 +12,5 @@ exports.run = (msg,client,Discord,color,arg1) => {
         .setFooter(`Requested by ${msg.author.username}`,msg.author.avatarURL)
         .setColor(color);
         msg.channel.send(im)
-       /* msg.channel.send({embed : {
-             image: {
-                 url: img[ran].url
-             }
-        }});*/
   })
 }
