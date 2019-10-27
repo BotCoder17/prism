9d6996b66ab4a45452985404d98bd7db2f518f9f
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 02:36:53 +0530

    Update google.js

--- google.js
+++ google.js
@@ -14,27 +14,12 @@ exports.run = (msg,Discord,client,color,arg1) => {
      
     for (var i = 0; i < res.length; i++) {
        // t.addField('\u200B',`[${res[i].title}](${res[i].link})`)
-        let tit='',ti=res[i].title, lk=res[i].link,lnk='';
-        for(var j=0;j<ti.length;j++){
-           if(ti.charAt(j)!=')'&&ti.charAt(j)!='('&&ti.charAt(j)!=']'&&ti.charAt(j)!='['){
+        let tit='',ti=res[i].title;
+        for(var j=0;j<ti.length;j++)
+           if(ti.charAt(j)!=')'&&ti.charAt(j)!='('&&ti.charAt(j)!=']'&&ti.charAt(j)!='[')
                tit+=String(ti.charAt(j));
-           }
-        }
-       for(var j=0;j<lk.length;j++){
-           if((lk.charAt(j)!=')'&&lk.charAt(j)=='(')){
-               lnk+=String(ti.charAt(j));
-               bl=true;
-           }
-          if(lk.charAt(j)!=']'&&lk.charAt(j)=='['){
-             lnk+=String(ti.charAt(j));
-               bl2=true;
-          }
-        }
-       if(bl==true) lnk+=')';
-       if(bl2==true) lnk+=']';
-       t.addField('\u200B',`[${tit}](${lnk})`)
+       t.addField('\u200B',`[${tit}](${res[i].link})`)
        tit='';
-       lnk='';
     }
     msg.channel.send(t)
   }).catch(err => {
