df1e01aff80afc8f9bdaefac78b0963fdaef6405
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 01:04:57 +0530

    Update image.js

--- image.js
+++ image.js
@@ -1,4 +1,4 @@
-exports.run = (msg,Discord,client,color,arg1) => {
+exports.run = (msg,client,Discord,color,arg1) => {
     const gis = require('g-i-s');
     gis(arg1, (err, res) => {
        if(err) return;
@@ -6,10 +6,17 @@ exports.run = (msg,Discord,client,color,arg1) => {
      //  console.log(JSON.parse(JSON.stringify(res, null, '  ')));
        let img=JSON.parse(JSON.stringify(res, null, '  '));
         console.log(arg1)
-        msg.channel.send({embed : {
+        let im=new Discord.RichEmbed()
+        .setTitle(`Image search for ${arg1}`)
+        .setImage(img[ran].url)
+        .setTimestamp(new Date())
+        .setFooter(`Requested by ${msg.author.username}`,msg.author.avatarURL)
+        .setColor(color);
+        msg.channel.send(im)
+       /* msg.channel.send({embed : {
              image: {
                  url: img[ran].url
              }
-        }});
+        }});*/
   })
 }
