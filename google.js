7c65aefdc3da9c36adeb7f5c4fd7d381225657e3
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 02:23:37 +0530

    Update google.js

--- google.js
+++ google.js
@@ -11,7 +11,15 @@ exports.run = (msg,Discord,client,color,arg1) => {
         .setFooter(`Requested by ${msg.author.username}`,msg.author.avatarURL)
     
     for (var i = 0; i < res.length; i++) {
-        t.addField('\u200B',`[${res[i].title}](${res[i].link})`)
+       // t.addField('\u200B',`[${res[i].title}](${res[i].link})`)
+        let tit='',ti=res[i].title;
+        for(var j=0;j<ti.length;j++){
+           if(ti.charAt(j)!=')'&&ti.charAt(j)!='('&&ti.charAt(j)!=']'&&ti.charAt(j)!='['){
+               tit+=String(ti.charAt(j));
+           }
+        }
+       t.addField('\u200B',`[${tit}](${res[i].link})`)
+       tit='';
     }
     msg.channel.send(t)
   }).catch(err => {
