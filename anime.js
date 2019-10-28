1dc8ed9df6ee8b8dff9ff4af56a59a92c9154ee4
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 15:14:48 +0530

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,6 +1,12 @@
 exports.run = (msg,Discord,client,color,cmd,arg01) => {
    const weeb = require('node-weeb');
    const sr = require('./sfw.json');
+  /*
+      "foxGirl": "fox_girl",
+      "kemonomimi": "kemonomimi",
+      "holo": "holo",
+      "woof": "woof"
+  */
    if(arg01!=''){
      client.fetchUser(arg01)
       .then(u => {
@@ -17,7 +23,67 @@ exports.run = (msg,Discord,client,color,cmd,arg01) => {
                   msg.channel.send(r);
                }).catch(console.log);
           break;
-          case 'feed':
+          case 'slap':
+            weeb.nekoLife(sr.sfw.slap, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} slapped u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+         break;
+         case 'poke':
+            weeb.nekoLife(sr.sfw.poke, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} poked u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+         break;
+         case 'pat':
+           weeb.nekoLife(sr.sfw.pat, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} pats u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+         break;
+         case 'kiss':
+            weeb.nekoLife(sr.sfw.kiss, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} kissed u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+         break;
+         case 'hug':
+            weeb.nekoLife(sr.sfw.hug, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} hugged u, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+         break;
+         case 'feed':
                weeb.nekoLife(sr.sfw.feed, 'img')
                .then(res => {
                   let r=new Discord.RichEmbed()
@@ -58,7 +124,58 @@ exports.run = (msg,Discord,client,color,cmd,arg01) => {
                      msg.channel.send(r);
                }).catch(console.log);
           break;
+          case 'smug':
+            weeb.nekoLife(sr.sfw.smug, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} just smugged, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+          break;
+          case 'baka':
+            weeb.nekoLife(sr.sfw.baka, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`${msg.author.username} called u baka, ${u.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+          break;
         }
      }).catch(console.log);
+   }else{
+     switch(cmd){
+       case 'neko':
+         weeb.nekoLife(sr.sfw.neko, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`Here's a neko for u, ${msg.author.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+       break;
+       case 'meow':case 'cat':
+            weeb.nekoLife(sr.sfw.meow, 'img')
+               .then(res => {
+                  let r=new Discord.RichEmbed()
+                 .setTimestamp(new Date())
+                 .setTitle(`Meow! ${msg.author.username}`)
+                 .setFooter(`from ${msg.author.username}`,msg.author.avatarURL)
+                 .setImage(JSON.parse(res).url)
+                 .setColor(color);
+                  msg.channel.send(r);
+               }).catch(console.log);
+          break;
+      }
    }
 }
