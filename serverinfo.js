0dd0596aefaa0237834a7031658669c5a2a1a32a
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 14:40:26 +0530

    Create serverinfo.js

---
+++ serverinfo.js
@@ -0,0 +1,15 @@
+exports.run = (msg,client,Discord,color) => {
+      let si=new Discord.RichEmbed()
+     .setTitle('Server Info')
+     .addField('Name',msg.guild.name)
+     .addField('ID',msg.guild.id)
+     .addField('Owner',<@${msg.guild.ownerID}>)
+     .addField('Created At',new Date(msg.guild.createdTimestamp).toGMTString())
+     .addField('Region',msg.guild.region)
+     .addField('Member Count',msg.guild.memberCount)
+     .setThumbnail(msg.guild.iconURL)
+     .setColor(color)
+     .setFooter(`Requested by ${msg.author.username}`,msg.author.avatarURL)
+
+     msg.channel.send(si)
+}
