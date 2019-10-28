52da03ab4d4e1b7bbe814968bbe4cbf19a384099
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-28 15:48:40 +0530

    Create wiki.js

---
+++ wiki.js
@@ -0,0 +1,17 @@
+exports.run = (msg,client,Discord,color,arg1) => {
+     const Intlpedia = require('intl-wikipedia')
+     const searchTerm = arg1
+     const intlpedia = new Intlpedia('en')
+intlpedia.search(searchTerm)
+  .then(page => {
+        //console.log(page);
+        let sumry=page.summary,sumres='';
+        for(var i=0;i<1800;i++){
+          sumres+=String(sumry.charAt(i));
+        }
+        let wik=new Discord.RichEmbed()
+        .setTitle(page.name)
+        .setDescription(`${sumres} [Read more...](https://en.wikipedia.org/wiki/${page.name})`)
+        msg.channel.send(wik)
+  }).catch(err => console.error(err))
+}
