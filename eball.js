3ea9db827e2fe94707377e66f61380ce6c9c7906
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-29 16:25:33 +0530

    Update eball.js

--- eball.js
+++ eball.js
@@ -1,14 +1,18 @@
 exports.run = (msg,client,Discord,color,arg1) => {
    const weeb = require('node-weeb');
    const nekoLifeJSON = require('./sfw.json');
-   weeb.nekoLife(nekoLifeJSON.sfw.eBall, 'img').then(res => {
-       var data = JSON.parse(res).url;
-       let r=new Discord.RichEmbed()
-      .setAuthor(`Question by ${msg.author.username}`, msg.author.avatarURL)
-      .setDescription(`\`\`\`${arg1}\`\`\``)
-      .setImage(data)
-      .setTimestamp(new Date())
-      .setColor(color);
-      msg.channel.send(r);
+   if(arg1!=''){
+      weeb.nekoLife(nekoLifeJSON.sfw.eBall, 'img').then(res => {
+         var data = JSON.parse(res).url;
+         let r=new Discord.RichEmbed()
+         .setAuthor(`Question by ${msg.author.username}`, msg.author.avatarURL)
+         .setDescription(`\`\`\`${arg1}\`\`\``)
+         .setImage(data)
+         .setTimestamp(new Date())
+         .setColor(color);
+          msg.channel.send(r);
   }).catch(console.log);
+  }else{
+     msg.channel.send('Any question?');
+   }
 }
