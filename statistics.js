6b7738f5448736296ec1e17cbbd62c21a71fde18
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-30 23:49:48 +0530

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -1,5 +1,13 @@
 exports.run = (msg,client,Discord,color,ownerID) => {
    if(ownerID==msg.author.id){
+      // versions
+      let f=parseInt(0),s=parseInt(1),ver="";
+      var vs = process.versions
+      var res = [];
+      for(var i in vs)
+          res.push([i, vs[i]]);
+      for(var i=0;i<res.length;i++)
+          ver+=`${res[f]} : v${res[s]}\n`;
       //memory usage
       let rss=(process.memoryUsage().rss/1e+6),heapTotal=(process.memoryUsage().heapTotal/1e+6),    //all in MB
           heapUsed=(process.memoryUsage().heapUsed/1e+6),external=(process.memoryUsage().external/1e+6);
@@ -8,8 +16,8 @@ exports.run = (msg,client,Discord,color,ownerID) => {
       //uptime
       let uptime=(Math.floor(process.uptime())/60); // mins
       let st=new Discord.RichEmbed()
-             .setTitle('Statistic 📊')
-             .addField('Node version',process.version)
+             .setTitle('Statistics 📊')
+             .addField('Version',`\`${ver}\``)
              .addField('Memory usage',`**Resident Set Size** : \`${rss} MB\`\n**Total memory remaining** : \`${heapTotal} MB\`\n**Total memory used** : \`${heapUsed} MB\`\n**Memory usage of C++ objects** : \`${external} MB\``)
              .addField('CPU Usage',`**User** : \`${user} mins\`\n**System** : \`${system} mins\``)
              .addField('Uptime',`\`${uptime} mins\``)
