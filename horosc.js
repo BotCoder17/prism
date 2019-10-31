ba6cc5f9aad3b8dccfd0e7e34eb6e402776ab53e
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-31 20:03:35 +0530

    Update horosc.js

--- horosc.js
+++ horosc.js
@@ -1,14 +1,21 @@
 exports.run = (msg,client,Discord,color,arg1) => {
-    const aztroJs = require("aztro-js")
-    let sign = arg1.toLowerCase();
-     aztroJs.getAllHoroscope(sign, function(res) {
+    const aztroJs = require("aztro-js");
+    let zds=["Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpius","Sagittarius","Capricornus","Aquarius","Pisces"];
+    let sign = arg1.toLowerCase().trim();
+    var bl=false;
+    for(var i=0;i<zds.length;i++){
+        if(sign==zds[i].toLowerCase()){
+            bl=true;
+            break;
+        }
+    }
+    if(bl==true){
+    aztroJs.getAllHoroscope(sign, function(res) {
          try{
              let r=res;
              let cd=r.yesterday.current_date;
              let cd2=r.today.current_date;
              let cd3=r.tomorrow.current_date;
-             if(r.today.description=="undefined"||r.tomorrow.description=="undefined"||r.yesterday.description=="undefined"){ return;}
-             else{
                 let hor=new Discord.RichEmbed()
                    .setTitle(`Horoscope of ${arg1}`)
                    .setColor(color)
@@ -18,9 +25,11 @@ exports.run = (msg,client,Discord,color,arg1) => {
                    .addField('Tomorrow',`\`\`\`${r.tomorrow.description}\`\`\`\nDate : \`${cd3}\`\nLucky Number : \`${r.tomorrow.lucky_number}\`\nLucky Time : \`${r.tomorrow.lucky_time}\`\nCompatibility : \`${r.tomorrow.compatibility}\`\nMood : \`${r.tomorrow.mood}\`\nColor : \`${r.tomorrow.color}\``)
                    .addField('Yesterday',`\`\`\`${r.yesterday.description}\`\`\`\nDate : \`${cd}\`\nLucky Number : \`${r.yesterday.lucky_number}\`\nLucky Time : \`${r.yesterday.lucky_time}\`\nCompatibility : \`${r.yesterday.compatibility}\`\nMood : \`${r.yesterday.mood}\`\nColor : \`${r.yesterday.color}\``)
                msg.channel.send(hor)
-             }
           }catch(err){
               msg.channel.send(`Couldn't find horoscope info for \`${arg1}\``);
           }
      });
+    }else{
+       msg.channel.send(`Incorrect zodiac name!`);
+    }
 }
