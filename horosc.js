a44efb47744ba30183ad89034b90b1fb053ea19d
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-31 19:30:53 +0530

    Update horosc.js

--- horosc.js
+++ horosc.js
@@ -13,9 +13,9 @@ exports.run = (msg,client,Discord,color,arg1) => {
                    .setColor(color)
                    .setTimestamp(new Date())
                    .setFooter(`For ${msg.author.username}`,msg.author.avatarURL)
-                   .addField('Today',`Summary : \`${r.today.description}\`\nDate : \`${cd2}\`\nLucky Number : \`${r.today.lucky_number}\`\nLucky Time : \`${r.today.lucky_time}\`\n\nCompatibility : \`${r.today.compatibility}\`\nMood : \`${r.today.mood}\`\nColor : \`${r.today.color}\``)
-                   .addField('Tomorrow',`Summary : \`${r.tomorrow.description}\`\nDate : \`${cd3}\`\nLucky Number : \`${r.tomorrow.lucky_number}\`\nLucky Time : \`${r.tomorrow.lucky_time}\`\n\nCompatibility : \`${r.tomorrow.compatibility}\`\nMood : \`${r.tomorrow.mood}\`\nColor : \`${r.tomorrow.color}\``)
-                   .addField('Yesterday',`Summary : \`${r.yesterday.description}\`\nDate : \`${cd}\`\nLucky Number : \`${r.yesterday.lucky_number}\`\nLucky Time : \`${r.yesterday.lucky_time}\`\n\nCompatibility : \`${r.yesterday.compatibility}\`\nMood : \`${r.yesterday.mood}\`\nColor : \`${r.yesterday.color}\``)
+                   .addField('Today',`\`\`\`${r.today.description}\`\`\`\nDate : \`${cd2}\`\nLucky Number : \`${r.today.lucky_number}\`\nLucky Time : \`${r.today.lucky_time}\`\nCompatibility : \`${r.today.compatibility}\`\nMood : \`${r.today.mood}\`\nColor : \`${r.today.color}\``)
+                   .addField('Tomorrow',`\`\`\`${r.tomorrow.description}\`\`\`\nDate : \`${cd3}\`\nLucky Number : \`${r.tomorrow.lucky_number}\`\nLucky Time : \`${r.tomorrow.lucky_time}\`\nCompatibility : \`${r.tomorrow.compatibility}\`\nMood : \`${r.tomorrow.mood}\`\nColor : \`${r.tomorrow.color}\``)
+                   .addField('Yesterday',`\`\`\`${r.yesterday.description}\`\`\`\nDate : \`${cd}\`\nLucky Number : \`${r.yesterday.lucky_number}\`\nLucky Time : \`${r.yesterday.lucky_time}\`\nCompatibility : \`${r.yesterday.compatibility}\`\nMood : \`${r.yesterday.mood}\`\nColor : \`${r.yesterday.color}\``)
                msg.channel.send(hor)
           }catch(err){
               msg.channel.send(`Couldn't find horoscope info for \`${arg1}\``);
