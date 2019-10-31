3233b3c2cc241fddd5869b2eaeed99acec2e4592
idris <50110894+BotCoder17@users.noreply.github.com>
2019-10-31 19:55:05 +0530

    Update horosc.js

--- horosc.js
+++ horosc.js
@@ -7,8 +7,9 @@ exports.run = (msg,client,Discord,color,arg1) => {
              let cd=r.yesterday.current_date;
              let cd2=r.today.current_date;
              let cd3=r.tomorrow.current_date;
-             if(r.today.description=="undefined"||r.tomorrow.description=="undefined"||r.yesterday.description=="undefined") return;
-             let hor=new Discord.RichEmbed()
+             if(r.today.description=="undefined"||r.tomorrow.description=="undefined"||r.yesterday.description=="undefined"){ return;}
+             else{
+                let hor=new Discord.RichEmbed()
                    .setTitle(`Horoscope of ${arg1}`)
                    .setColor(color)
                    .setTimestamp(new Date())
@@ -17,6 +18,7 @@ exports.run = (msg,client,Discord,color,arg1) => {
                    .addField('Tomorrow',`\`\`\`${r.tomorrow.description}\`\`\`\nDate : \`${cd3}\`\nLucky Number : \`${r.tomorrow.lucky_number}\`\nLucky Time : \`${r.tomorrow.lucky_time}\`\nCompatibility : \`${r.tomorrow.compatibility}\`\nMood : \`${r.tomorrow.mood}\`\nColor : \`${r.tomorrow.color}\``)
                    .addField('Yesterday',`\`\`\`${r.yesterday.description}\`\`\`\nDate : \`${cd}\`\nLucky Number : \`${r.yesterday.lucky_number}\`\nLucky Time : \`${r.yesterday.lucky_time}\`\nCompatibility : \`${r.yesterday.compatibility}\`\nMood : \`${r.yesterday.mood}\`\nColor : \`${r.yesterday.color}\``)
                msg.channel.send(hor)
+             }
           }catch(err){
               msg.channel.send(`Couldn't find horoscope info for \`${arg1}\``);
           }
