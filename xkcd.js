71603d8380d3e54c13230d88805d5c8fea56ad1e
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-01 20:32:38 +0530

    Create xkcd.js

---
+++ xkcd.js
@@ -0,0 +1,17 @@
+exports.run = (msg,client,Discord,color) => {
+    var xkcd = require('xkcd');
+    let opts = Math.floor(Math.random()*2000)
+    xkcd(opts,function (data) {
+        let xc=new Discord.RichEmbed()
+        .setTitle(data.title);
+        if(data.alt!=''){
+           xc.setDescription(data.alt)
+        }
+        xc.setImage(data.img)
+        .setColor(color)
+        .setFooter(`Xkcd for ${msg.author.username}`,msg.author.avatarURL)
+        .setTimestamp(new Date())
+        
+        msg.channel.send(xc)
+    });
+}
