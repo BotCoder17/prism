961ea60e0fb4126dbeb73a10bc6af866d87259e6
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-01 19:31:37 +0530

    Create ytsearch.js

---
+++ ytsearch.js
@@ -0,0 +1,13 @@
+exports.run = (msg,client,Discord,arg1) => {
+    const ytSearch = require('yt-search')
+    const opts = {
+      query: arg1,
+      pageStart: 1, 
+      pageEnd: 3
+    };
+    ytSearch(opts, function (err,r) {
+       if (err) throw err;
+       const res = r.videos[Math.floor(Math.random()*(r.videos.length))]
+       msg.channel.send(`https://www.youtube.com${res.url}`)
+    });
+}
