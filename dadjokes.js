4c0dfe94e08b37807293277ae7cc505d24d9be1a
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-01 19:04:38 +0530

    Create dadjokes.js

---
+++ dadjokes.js
@@ -0,0 +1,14 @@
+exports.run = (msg,client,Discord,color) => {
+   const DadJokes = require('dadjokes-wrapper');
+   const dj = new DadJokes();
+ 
+   dj.randomJoke()
+     .then(res => { 
+         let jk=new Discord.RichEmbed()
+         .setTitle('Joke')
+         .setDescription(res)
+         .setColor(color)
+         .setTimestamp(new Date())
+         .setFooter(`Joke for ${msg.author.username}`,msg.author.avatarURL)
+     }).catch(err => console.error(err));
+}
