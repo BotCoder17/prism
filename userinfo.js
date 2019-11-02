2c8cd48666e90288fb188321e0b67d978f4eaff5
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-02 14:59:28 +0530

    Update userinfo.js

--- userinfo.js
+++ userinfo.js
@@ -2,6 +2,15 @@ exports.run = (msg,client,Discord,color,arg01) => {
     if(arg01!=''){
        client.fetchUser(arg01)
          .then(u => {
+           let rls="";
+           try{
+               msg.guild.members.get(u.id).roles.map(r => {
+               if(r.id!=msg.guild.id)
+                   rls+=`<@&${r.id}>  `
+               else
+                  rls+=`${r.name}  `
+               })
+           }catch(err){}
              let csd=new Discord.RichEmbed()
              .setTitle('User Info')
              .setColor(color)
@@ -9,12 +18,13 @@ exports.run = (msg,client,Discord,color,arg01) => {
              .addField('Name',u.username)
              .addField('ID',u.id)
              .addField('Tag',u.tag)
-             .addField('Status',u.presence.status);
+             if(rls!=""){
+                csd.addField('Roles',rls)
+             }
+             csd.addField('Status',u.presence.status);
              try{
                csd.addField(`Joined ${msg.guild.name} on`,new Date(msg.guild.members.get(arg01).joinedTimestamp).toGMTString());
-             }catch(err){
-               console.log(err);
-             }
+             }catch(err){}
              csd.addField('Joined Discord on',new Date(u.createdTimestamp).toGMTString())
              .addField('Is a bot?',(u.bot==true)?'Yes':'No');
              msg.channel.send(csd);
