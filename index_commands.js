568d05f834ccb1104b165a748ddec68fd6052986
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-03 14:18:23 +0530

    Update index_commands.js

--- index_commands.js
+++ index_commands.js
@@ -5,33 +5,33 @@ exports.run = (msg,client,Discord,pf,color,arg0,arg01,arg1,cmd,pf2,ownerID,gif_a
   }
 /*****************/
  else if(pf2.toLowerCase()==pf.toLowerCase()){
-    let react=require('./reaction.js');
+    let react=require('./react/reaction.js');
     react.run(msg,Discord,client,color,cmd,arg01);
-    let memes=require('./memes.js');
+    let memes=require('./lolmemes/memes.js');
     memes.run(msg,client,Discord,cmd,color,arg1);
     switch(cmd){
         case 'ping':
-            let ping=require('./ping.js');
+            let ping=require('./utility/ping.js');
             ping.run(Discord,client,msg);
         break;
         case 'stat':case 'stats':case 'statistic':case 'statistics':
-            let stat=require('./statistics.js');
+            let stat=require('./owner/statistics.js');
             stat.run(msg,client,Discord,color,ownerID);
         break;
         case 'xkcd':case 'xd':
-           let xkcds=require('./xkcd.js');
+           let xkcds=require('./fun/xkcd.js');
            xkcds.run(msg,client,Discord,color);
         break;
         case 'searchgif':case 'sgif':case 'gifsearch':
-           let giff=require('./sgif.js');
+           let giff=require('./search/sgif.js');
            giff.run(msg,client,Discord,color,arg1,gif_api);
         break;
         case 'joke':case 'jokes':
-           let jks=require('./dadjokes.js');
+           let jks=require('./fun/dadjokes.js');
            jks.run(msg,client,Discord,color);
         break;
         case 'yt':case 'youtube':
-           let yts=require('./ytsearch.js');
+           let yts=require('./search/ytsearch.js');
            yts.run(msg,client,Discord,arg1);
         break;
         case 'help':
@@ -39,71 +39,71 @@ exports.run = (msg,client,Discord,pf,color,arg0,arg01,arg1,cmd,pf2,ownerID,gif_a
             help.run(msg,client,Discord,cmd,color,arg1);
         break;
         case 'img':case 'image':
-            let img=require('./image.js');
+            let img=require('./search/image.js');
             img.run(msg,client,Discord,color,arg1);
         break;
         case 'horoscope':case 'horc':case 'hrc':
-            let hrc=require('./horosc.js');
+            let hrc=require('./general/horosc.js');
             hrc.run(msg,client,Discord,color,arg1);
         break;
         case 'google':case 'ggl':
-            let google=require('./google.js');
+            let google=require('./search/google.js');
             google.run(msg,Discord,client,color,arg1);
         break;
         case 'fact':case 'facts':
-            let fcts=require('./fct.js');
+            let fcts=require('./fun/fct.js');
             fcts.run(msg,client,Discord,color);
         break;
         case 'trn': case 'translate':
-            let trn=require('./trn.js');
+            let trn=require('./general/trn.js');
             trn.run(msg,Discord,client,color,arg1);
         break;
         case 'dict': case 'dictionary':
-            let dict=require('./gendic.js');
+            let dict=require('./search/gendic.js');
             dict.run(msg,client,Discord,color,arg1,pf);
         break;
         case 'wiki':case 'wikipedia':
-            let wik=require('./wiki.js');
+            let wik=require('./search/wiki.js');
             wik.run(msg,client,Discord,color,arg1);
         break;
         case 'ev': case 'eval':
-            let eval=require('./eval.js');
+            let eval=require('./owner/eval.js');
             eval.run(msg,client,Discord,color,ownerID);
         break;
         case 'emoji':
-            let emoji=require('./emoji.js');
+            let emoji=require('./utility/emoji.js');
             emoji.run(msg,Discord,client,color,arg1);
         break;
         case 'weather':
-            let weather=require('./temp.js');
+            let weather=require('./search/temp.js');
             weather.run(msg,client,color,Discord,arg1);
         break;
         case 'say':
-            let say=require('./say.js');
+            let say=require('./utility/say.js');
             say.run(msg,arg1);
         break;
         case 'ub': case 'urban':
-            let urban=require('./urban.js');
+            let urban=require('./search/urban.js');
             urban.run(msg,Discord,client,color,arg1);
         break;
         case 'res':case 'restart':
-            let rb=require('./restart.js');
+            let rb=require('./owner/restart.js');
             rb.run(msg,client,Discord,color,pf,ownerID);
         break;
         case 'ava':case 'avatar':
-            let ava=require('./ava.js');
+            let ava=require('./utility/ava.js');
             ava.run(msg,client,Discord,color,arg01);
         break;
         case 'whois':case 'userinfo':
-            let whois=require('./userinfo.js');
+            let whois=require('./utility/userinfo.js');
             whois.run(msg,client,Discord,color,arg01);
         break;
         case 'serverinfo':case 'whatis':
-            let serv=require('./serverinfo.js');
+            let serv=require('./utility/serverinfo.js');
             serv.run(msg,client,Discord,color);
         break;
         case '8ball':case '8b':
-            let eball=require('./eball.js');
+            let eball=require('./fun/eball.js');
             eball.run(msg,client,Discord,color,arg1);
         break;
     }
