9dc640a0dd12e003f39f231aa53bb036c1257c2e
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-07 21:21:20 +0530

    Update ytsearch.js

--- search/ytsearch.js
+++ search/ytsearch.js
@@ -7,6 +7,30 @@ exports.run = (msg,client,Discord,arg1) => {
     };
     ytSearch(opts, function (err,r) {
        if (err) throw err;
+       const Pagination = require('discord-paginationembed');
+         const embeds = [];
+         for (let i = 0; i < r.videos.length; i++)
+         embeds.push(new Discord.RichEmbed()
+                     .setTitle(r.videos[i].title).setURL(`https://www.youtube.com${r.videos[i].url}`)
+                     .addField('Viewers',r.videos[i].views)
+                     .addField('Posted',r.videos[i].ago)
+                     .addField('Duration',r.videos[i].timestamp));
+         new Pagination.Embeds()
+              .setArray(embeds)
+              .setAuthorizedUsers([msg.author.id])
+              .setChannel(msg.channel)
+              .setPageIndicator(true)
+              .setColor('#c4302b')
+              .setPage(1)
+              .setTitle(`Image search for ${arg1}`)
+              .setTimeout(69000)
+              .setNavigationEmojis({
+                  back: '◀',
+                  forward: '▶',
+                  jump: '🔢',
+                  delete: '🗑'
+               })
+              .build();
        console.log(r)
        const res = r.videos[Math.floor(Math.random()*(r.videos.length))]
        msg.channel.send(`https://www.youtube.com${res.url}`)
