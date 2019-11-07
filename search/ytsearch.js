b41d0744fcd6d345ac090d73e6c37584e53d9907
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-07 21:03:51 +0530

    Update ytsearch.js

--- search/ytsearch.js
+++ search/ytsearch.js
@@ -7,6 +7,7 @@ exports.run = (msg,client,Discord,arg1) => {
     };
     ytSearch(opts, function (err,r) {
        if (err) throw err;
+       console.log(r)
        const res = r.videos[Math.floor(Math.random()*(r.videos.length))]
        msg.channel.send(`https://www.youtube.com${res.url}`)
     });
