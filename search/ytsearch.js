aa682bcf70329bf5b9330accd8ab033c1ed246b3
idris <50110894+BotCoder17@users.noreply.github.com>
2019-11-08 00:05:54 +0530

    Update ytsearch.js

--- search/ytsearch.js
+++ search/ytsearch.js
@@ -9,19 +9,15 @@ exports.run = (msg,client,Discord,arg1) => {
     ytSearch(opts, function (err,r) {
        if (err) throw err;
        const Pagination = require('discord-paginationembed');
-         const embeds = [];let vid='';
+         const embeds = [];
          for (let i = 1; i < r.videos.length; i++){
-             let vd=r.videos[i];
-             getThumb(`https://www.youtube.com${vd.url}`)
-               .then(thumb_url => {
-                  vid=thumb_url;
+             let vd=r.videos[i]
              embeds.push(new Discord.RichEmbed()
                          .setImage(vid)
-                         .setTitle(vd.title).setURL(`https://www.youtube.com${vd.url}`)
+                         .setTitle(vd.title).setURL(getThumb(`https://www.youtube.com${vd.url}`).then((tmb) => tmb))
                          .addField('Views',vd.views,true)
                          .addField('Posted',vd.ago,true));
-         }).catch(console.log);
-     }
+         }
          new Pagination.Embeds()
               .setArray(embeds)
               .setAuthorizedUsers([msg.author.id])
